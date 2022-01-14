b06a54573c52b4fb4e4967163deaf7c66eb555f3
Gaston Irby III <gaston@Gastons-iMac.hsd1.wa.comcast.net>
2022-01-14 09:35:37 -0800

    added useform

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
